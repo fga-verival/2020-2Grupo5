615f130a67962ce00fcc0daef4bde93d2067fa7a
Luis Gustavo <luis.gustavo3013@hotmail.com>
2021-04-19 20:10:16 -0300

    [Rendimento Bruto] 1o. commit

---
+++ src/test/TestRendimentoBruto.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestRendimentoBruto {
+	
+	float valorInicial;
+	float taxaJuros;
+	int qtdeDias;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testRendimentoBruto1() {
+		valorInicial = 1000f;
+		taxaJuros = 0.085f;
+		qtdeDias = 60;
+		
+		CDB cdb = new CDB(valorInicial, taxaJuros, qtdeDias);
+		
+		float rendimentoBruto = cdb.getRendimentoBruto();
+		
+		assertEquals(rendimentoBruto, 13.97f);
+	}
+
+}
