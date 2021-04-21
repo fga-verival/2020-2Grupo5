b893dea95ed27bc3e46bf7ec551b1727e877860b
Luis Gustavo <luis.gustavo3013@hotmail.com>
2021-04-20 22:53:58 -0300

    [Rendimento Liquido] 1o. commit

---
+++ src/test/TestRendimentoLiquido.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import app.CDB;
+
+class TestRendimentoLiquido {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testRendimentoLiquido1() {
+		float valorInicial = 1000f;
+		float taxaJuros = 0.085f;
+		int qtdeDias = 60;
+		
+		CDB cdb = new CDB(valorInicial, taxaJuros, qtdeDias);
+		
+		float rendimentoLiquido = cdb.getRendimentoLiquido();
+		
+		assertEquals(rendimentoLiquido, 1.0829f);
+	}
+
+}
